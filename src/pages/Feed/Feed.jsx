5ead2a0f6c31bf86e5a96a2852de95c6aca9c27b
Zdthns <Zdthns25@gmail.com>
2023-01-22 22:06:39 +0300

    start

---
+++ src/pages/Feed/Feed.jsx
@@ -0,0 +1,40 @@
+import { React, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import style from "./style.module.css";
+import Orders from "../../components/feedComponents/Orders/Orders";
+import OrdersStatus from "../../components/feedComponents/OrderStatus/OrderStatus";
+import {
+  wsConnectionClose,
+  wsConnectionClosed,
+  wsConnectionStart,
+} from "../../services/actions/wsConect";
+import { useLocation, useParams } from "react-router-dom";
+import { wsUrl } from "../../utils/userApi.js";
+
+function Feed() {
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(wsConnectionStart(wsUrl));
+
+    return () => {
+      dispatch(wsConnectionClosed());
+    };
+  }, [dispatch, location]);
+
+  const orders = useSelector((store) => store.wsReducer.messages.orders);
+  return (
+    <section className={style.page}>
+      <article className={`pl-2 pr-2 ${style.feed_section}`}>
+        <h1 className="text text_type_main-large mt-10 mb-5">Лента заказов</h1>
+        <div className={`mt-5 ${style.section}`}>
+          <Orders data={orders} />
+        </div>
+      </article>
+      <OrdersStatus />
+    </section>
+  );
+}
+
+export default Feed;
---
+++ src/utils/types/types.ts
@@ -0,0 +1,36 @@
+import { CLOSE_INGREDIENT_DETAILS, CLOSE_ORDER_DETAILS, OPEN_INGREDIENT_DETAILS, OPEN_ORDER_DETAILS } from "../../services/actions/modals";
+import { WS_USER_CONNECTION_START, WS_USER_CONNECTION_SUCCESS, WS_USER_CONNECTION_CLOSE, WS_USER_CONNECTION_ERROR, WS_USER_GET_MESSAGE, WS_USER_SEND_MESSAGE, WS_USER_CONNECTION_CLOSED, WS_CONNECTION_START, WS_CONNECTION_SUCCESS, WS_CONNECTION_ERROR, WS_CONNECTION_CLOSED, WS_GET_MESSAGE, WS_CONNECTION_CLOSE, WS_SEND_MESSAGE } from "../../services/actions/wsConect";
+
+export interface IformFields {
+  [n: string]: string
+}// name: string, placeholder: string, type: string
+type TStringFunc = () => string; // использовать для типизации любой функции без аргументов, которая возвращает строку
+export type TsArr = number | string;
+
+export type TIngredient = {
+  _id: string;
+  name: string;
+  type: string;
+  proteins: number;
+  fat: number;
+  carbohydrates: number;
+  calories: number;
+  price: number;
+  image: string;
+  image_mobile: string;
+  image_large: string;
+  __v: number;
+  key?: string
+  count?: number
+};
+
+
+export type initialStateConstructorType ={}
+export type initialStateIngredientType ={}
+export type initialStateIngredientsType ={}
+export type initialStateOrderType ={}
+export type initialStateWsType ={}
+
+
+
+
